08304d4faa170a7fe9c97e4782353fd4d03bfd7e
Heet raval <80830479+hardeeraval@users.noreply.github.com>
2024-12-05 16:23:46 -0500

    committed changes in UpdateExpenseActivity.java
    added logic to handle dropdown in xml

--- app/src/main/java/editExpense/UpdateExpenseActivity.java
+++ app/src/main/java/editExpense/UpdateExpenseActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +21,8 @@ import model.Expense;
 
 public class UpdateExpenseActivity extends AppCompatActivity {
 
-    private EditText etAmount, etCategory, etDate, etNotes, etPaymentMethod;
+    private EditText etAmount, etDate, etNotes;
+    private Spinner spinnerCategory, spinnerPaymentMethod;
     private int expensePosition;
     private Expense expense;
 
@@ -30,10 +33,22 @@ public class UpdateExpenseActivity extends AppCompatActivity {
 
         // Initialize views
         etAmount = findViewById(R.id.etAmount);
-        etCategory = findViewById(R.id.etCategory);
+        spinnerCategory = findViewById(R.id.spinnerCategory);
+        spinnerPaymentMethod = findViewById(R.id.spinnerPaymentMethod);
         etDate = findViewById(R.id.etDate);
         etNotes = findViewById(R.id.etNotes);
-        etPaymentMethod = findViewById(R.id.etPaymentMethod);
+
+        // Populate the Category Spinner with expense categories
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(this,
+                R.array.expense_categories, android.R.layout.simple_spinner_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(categoryAdapter);
+
+        // Populate the Payment Method Spinner with options
+        ArrayAdapter<CharSequence> paymentMethodAdapter = ArrayAdapter.createFromResource(this,
+                R.array.payment_methods, android.R.layout.simple_spinner_item);
+        paymentMethodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPaymentMethod.setAdapter(paymentMethodAdapter);
 
         // Retrieve the expense and position from the Intent
         Intent intent = getIntent();
@@ -51,10 +66,19 @@ public class UpdateExpenseActivity extends AppCompatActivity {
 
     private void populateFields(Expense expense) {
         etAmount.setText(expense.getAmount());
-        etCategory.setText(expense.getCategory());
+
+        // Set the selected category in the Spinner
+        ArrayAdapter<CharSequence> categoryAdapter = (ArrayAdapter<CharSequence>) spinnerCategory.getAdapter();
+        int categoryPosition = categoryAdapter.getPosition(expense.getCategory());
+        spinnerCategory.setSelection(categoryPosition);
+
         etDate.setText(expense.getDate());
         etNotes.setText(expense.getNotes());
-        etPaymentMethod.setText(expense.getPaymentMethod());
+
+        // Set the selected payment method in the Spinner
+        ArrayAdapter<CharSequence> paymentMethodAdapter = (ArrayAdapter<CharSequence>) spinnerPaymentMethod.getAdapter();
+        int paymentMethodPosition = paymentMethodAdapter.getPosition(expense.getPaymentMethod());
+        spinnerPaymentMethod.setSelection(paymentMethodPosition);
     }
 
     private void saveExpense() {
@@ -65,10 +89,10 @@ public class UpdateExpenseActivity extends AppCompatActivity {
 
         // Update the expense object
         expense.setAmount(etAmount.getText().toString());
-        expense.setCategory(etCategory.getText().toString());
+        expense.setCategory(spinnerCategory.getSelectedItem().toString()); // Get category from Spinner
         expense.setDate(etDate.getText().toString());
         expense.setNotes(etNotes.getText().toString());
-        expense.setPaymentMethod(etPaymentMethod.getText().toString());
+        expense.setPaymentMethod(spinnerPaymentMethod.getSelectedItem().toString()); // Get payment method from Spinner
 
         // Save updated expense list
         updateSharedPreferences(expense, expensePosition);
